e4a009ffc591f8e049e4be453f1538336064b8ed
Bluezly <141844841+Bluezly@users.noreply.github.com>
2026-08-09 02:12:00 +0000

    test: tighten similarity coverage

--- internal/similarity/similarity_test.go
+++ internal/similarity/similarity_test.go
@@ -11,9 +11,9 @@ import (
 	"testing"
 	"time"
 
-	"ciradar/internal/config"
-	"ciradar/internal/db"
-	"ciradar/internal/model"
+	"github.com/Bluezly/CIRadar/internal/config"
+	"github.com/Bluezly/CIRadar/internal/db"
+	"github.com/Bluezly/CIRadar/internal/model"
 )
 
 func TestLocalSimilarity(t *testing.T) {
