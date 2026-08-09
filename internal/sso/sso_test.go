f8f3b484eab99f5c7057cfd0f0b16e0bf92f14bb
Bluezly <141844841+Bluezly@users.noreply.github.com>
2026-08-09 02:12:08 +0000

    test: tighten sso coverage

--- internal/sso/sso_test.go
+++ internal/sso/sso_test.go
@@ -19,8 +19,8 @@ import (
 	"testing"
 	"time"
 
-	"ciradar/internal/config"
-	"ciradar/internal/model"
+	"github.com/Bluezly/CIRadar/internal/config"
+	"github.com/Bluezly/CIRadar/internal/model"
 )
 
 func TestProxyPrincipal(t *testing.T) {
