838308ea0e8c4eb8845a0173f0b816cf4a61b356
Bluezly <141844841+Bluezly@users.noreply.github.com>
2026-08-09 02:12:22 +0000

    marketplace: improve marketplace

--- internal/marketplace/marketplace.go
+++ internal/marketplace/marketplace.go
@@ -9,9 +9,9 @@ import (
 	"strings"
 	"time"
 
-	"ciradar/internal/config"
-	"ciradar/internal/db"
-	"ciradar/internal/model"
+	"github.com/Bluezly/CIRadar/internal/config"
+	"github.com/Bluezly/CIRadar/internal/db"
+	"github.com/Bluezly/CIRadar/internal/model"
 )
 
 type Account struct {
